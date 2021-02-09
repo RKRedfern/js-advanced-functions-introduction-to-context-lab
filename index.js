9d46740bb19fe31b60b205f216663119dd8e8b4c
RKRedfern <rkredfern@gmail.com>
2021-02-09 10:29:54 -0500

    commit tree a27a5b8f24ad3711666bdc17d7b226975cec26da

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function createEmployeeRecord(){
+
+}
+
+function createEmployeeRecords(){
+
+}
+
+function createTimeInEvent(){
+
+}
+
+function createTimeOutEvent(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function wagesEarnedOnDate(){
+
+}
+
+function allWagesFor(){
+
+}
+
+function findEmployeeByFirstName(){
+
+}
+
+function calculatePayroll(){
+    
+}
